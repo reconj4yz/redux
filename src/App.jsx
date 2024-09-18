4b93cb0a2debd5a936651e0d3094f4277a64be25
reconjayz <jaichawla263@gmail.com>
2024-09-19 00:54:43 +0530

    used redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment, incrementAsync, incrementByAmount } from './store/reducers/counterSlice'
 
 const App = () => {
+  const {value} = useSelector((state)=> state.counter)
+  const dispatch = useDispatch();
   return (
-    <div>App</div>
+    <div className='m-auto container mt-5 bg-blue-100 p-10'>
+      <h1 className='text-center font-bold text-3xl text-blue-900'>
+        Counter: {value}
+      </h1>
+      <div className='text-center mt-5'>
+        <button onClick={()=>dispatch(increment())} className='border-2 font-semibold border-blue-900 bg-transparent text-blue-900 px-5 py-2 rounded-md'>
+          Increment
+        </button>
+        <button onClick={()=>dispatch(decrement())} className='border-2 mx-5 font-semibold border-blue-900 bg-transparent text-blue-900 px-5 py-2 rounded-md'>
+          Decrement
+        </button>
+        <button onClick={()=>dispatch(incrementAsync(7))} className='border-2 font-semibold border-blue-900 bg-transparent text-blue-900 px-5 py-2 rounded-md'>
+          Increment By 7
+        </button>
+      </div>
+    </div>
   )
 }
 
---
+++ src/store/reducers/counterSlice.jsx
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  value: 0,
+}
+
+export const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+    increment: (state) => {
+      state.value += 1
+    },
+    decrement: (state) => {
+      state.value -= 1
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload
+    },
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export const incrementAsync = (amount) => (dispatch) => {
+    setTimeout(() => {
+      dispatch(incrementByAmount(amount))
+    }, 2000)
+  }
+export default counterSlice.reducer
